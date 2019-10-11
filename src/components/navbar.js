28115fd6753db1cbb06fb0d215545427f181078b
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-10-10 20:58:30 -0400

    Adds background change on navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import throttle from "lodash.throttle";
 
 import Logo from "../assets/logo.svg";
 import styles from "./navbar.module.css";
 
 function Navbar() {
+  const [isTransparent, setIsTransparent] = useState(true);
+  const handleScroll = useCallback(
+    throttle(() => {
+      const scrollPosition = window.scrollY;
+
+      if (scrollPosition >= 180 && isTransparent) {
+        setIsTransparent(false);
+      } else if (scrollPosition <= 180 && !isTransparent) {
+        setIsTransparent(true);
+      }
+    }),
+    [isTransparent]
+  );
+
+  useEffect(() => {
+    const onScroll = window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [handleScroll]);
+
   return (
-    <nav className="flex items-center justify-center py-3 sticky top-0 z-50">
+    <nav
+      className={`flex items-center justify-center py-3 sticky top-0 z-50 ${
+        !isTransparent ? `bg-gradient ${styles.shadow}` : ""
+      }`}
+    >
       <div className="flex justify-between w-11/12">
         <a href="/">
           <img className={styles.logo} src={Logo} alt="Fullstack Nights Logo" />
