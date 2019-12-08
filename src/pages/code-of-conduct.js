0e78690028f403162255dcc5a261822512537310
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-12-08 14:41:26 -0400

    Updates code of conduct

--- src/pages/code-of-conduct.js
+++ src/pages/code-of-conduct.js
@@ -16,70 +16,65 @@ export default function() {
       <SEO title="Code of Conduct" />
       <GradientBackground className="flex flex-col items-center justify-center text-center">
         <h2 className="text-h2 font-bold text-white">Code of conduct</h2>
-        <h4 className="text-h4 text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </h4>
       </GradientBackground>
       <PageSection title="Short version" className="mt-20">
         <div className="w-11/12 m-auto">
           <p className="text-rg">
-            Nuestra conferencia se dedica a ofrecer una experiencia libre de
-            abusos para todos, independientemente de su género, orientación
-            sexual, discapacidad, apariencia física, talla, raza o religión. No
-            toleramos abusos de los participantes de la conferencia en ninguna
-            de sus formas. El lenguaje e imágenes sexuales no son apropiados
-            para ninguna sala de la conferencia, incluyendo charlas, talleres,
-            fiestas, Twitter y otros medios online. Los participantes de la
-            conferencia que violen estas reglas pueden ser sancionados o
-            expulsados de la misma sin reembolso a discreción de los
-            organizadores.
+            Our conference is dedicated to offering a welcoming experience for
+            everyone, regardless of their gender, sexual orientation, ability,
+            appearance, ethnicity or religion. We do not tolerate abuses towards
+            the participants of the conference in any form. Sexual language
+            and/or sexualized images are not appropriate for any aspect of the
+            conference, including talks, workshops, gatherings, Twitter or any
+            other online medium associated to the event. Conference participants
+            who violate these rules could be sanctioned or expelled from the
+            conference without any reimbursement, at the discretion of the
+            conference organizers.
           </p>
         </div>
       </PageSection>
       <PageSection title="Long version">
         <div className="w-11/12 m-auto">
           <p className="text-rg mb-6">
-            Los abusos incluyen: comentarios ofensivos relativos a género,
-            orientación sexual, discapacidad, apariencia física, talla, raza,
-            religión, imágenes sexuales en espacios públicos, intimidación
-            deliberada, acoso, persecución, fotografía o grabación de abusos,
-            interrupciones continuadas de charlas u otros eventos, contacto
-            físico inapropiado y atención sexual no deseada.
+            Examples of abuses are: offensive comments regarding gender, sexual
+            orientation, ability, appearance, ethnicity or religion, sexual
+            images in public spaces, intimidation, harassment, recording or
+            photographing abuses, continued interruptions of the conference
+            talks or other events, inappropriate physical contact and sexual
+            harassment.
           </p>
           <p className="text-rg mb-6">
-            Se espera que los participantes instados a parar cualquier
-            comportamiento abusivo obedezcan inmediatamente.
+            We expect participants who are made aware of their abusive behavior
+            to immediately comply and stop said behavior.
           </p>
           <p className="text-rg mb-6">
-            Los patrocinadores también están sujetos a la política contra
-            abusos. En particular, los patrocinadores no deberían utilizar
-            imágenes, actividades u otro material con contenido sexual. El
-            personal de los puestos (voluntarios incluidos) no debería usar
-            ropa/uniformes/disfraces sexualizados, o que de otra manera crean un
-            ambiente sexualizado.
+            Conference sponsors are also subject to the policies against abuse.
+            In particular, sponsors should not make use of sexual content.
+            Conference personnel (volunteers included) should not make use of
+            clothing/uniforms/disguises that are sexual in nature.
           </p>
           <p className="text-rg mb-6">
-            Si un participante incurre en un comportamiento abusivo, los
-            organizadores de la conferencia pueden tomar cualquier acción que
-            estimen oportuna, incluyendo advertencias al ofensor o su expulsión
-            de la conferencia sin reembolso.
+            If a participant incurs in abusive behavior, the conference
+            personnel reserves the right to take official action, which may
+            include warnings as well as expulsion from the event without any
+            reimbursement.
           </p>
           <p className="text-rg mb-6">
-            Si estás sufriendo algún abuso, adviertes abusos sobre otra persona,
-            o tienes alguna otra preocupación, por favor contacta con algún
-            miembro del personal de la conferencia inmediatamente.
+            If you are suffering from abuse at the conference, know of anyone
+            suffering from abuse, or feel uncomfortable in any other way, please
+            contact any conference personnel immediately.
           </p>
           <p className="text-rg mb-6">
-            El personal de la conferencia estará contento de ayudar a los
-            participates a contactar con la seguridad del hotel/sala o con las
-            autoridades locales, proveer escolta o asistir de otra manera a
-            aquellos que estén sufriendo abusos para que se sientan seguros
-            mientras dure la conferencia. Valoramos tu asistencia.
+            The conference personnel will gladly help participants in contacting
+            the security personnel of the event location or the local
+            authorities, provide escort out of the conference, or otherwise
+            assist those who are the victims of abuse in any way so that they
+            can feel comfortable during the event. We value your assistance.
           </p>
           <p className="text-rg">
-            Esperamos que los participantes sigan estas reglas en la
-            conferencia, talleres y eventos sociales relacionados con la
-            conferencia.
+            We hope that participants follow these rules and regulations in the
+            conference, workshops and any other social event related to the
+            conference.
           </p>
         </div>
       </PageSection>
