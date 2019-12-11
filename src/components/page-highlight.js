a21d57b4dd97f11b4586fc53394e1f59305f624f
Emmanuel Luciano <emma.luciano11@gmail.com>
2019-12-10 20:11:48 -0400

    Updates code of conduct link and ui

--- src/components/page-highlight.js
+++ src/components/page-highlight.js
@@ -1,20 +1,29 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "gatsby";
 
 function PageHighlight({ header, subheader, url, urlTitle }) {
+  let isExternalUrl = url.indexOf("https") || url.indexOf("www");
+
   return (
     <section className="py-7 px-2 bg-primary lg:py-10 lg:px-10">
       <div className="m-auto text-center w-11/12 max-w-6xl lg:w-10/12">
         <h3 className="text-h3 text-white font-extrabold mb-4">{header}</h3>
         <h4 className="text-h4 text-white mb-6">{subheader}</h4>
-        <a
-          href={url}
-          className="btn btn--inverse lg:w-2/12"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {urlTitle}
-        </a>
+        {isExternalUrl > 0 ? (
+          <a
+            href={url}
+            className="btn btn--inverse md:w-3/12"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {urlTitle}
+          </a>
+        ) : (
+          <Link className="btn btn--inverse md:w-3/12" to={url}>
+            {urlTitle}
+          </Link>
+        )}
       </div>
     </section>
   );
