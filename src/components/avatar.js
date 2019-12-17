2d132566746e68a43ce1b5f284d6c9aef14d5b7f
Raúl Negrón <raul.esteban.negron@gmail.com>
2019-12-17 18:13:31 -0400

    Move margin to social mapping

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -17,13 +17,14 @@ export default function({ img, name, social = [] }) {
     <div className={styles.container}>
       <img className={`avatar m-auto ${styles.img}`} src={img} alt={name} />
       <p className="mt-3">{name}</p>
-      <div className="flex mt-3 justify-center">
+      <div className="flex justify-center">
         {social.map(s => {
           return (
+
             <a
               key={s.link}
               href={s.link}
-              className="w-8 mt-2"
+              className="w-8 mt-5"
               target="_blank"
               rel="noopener noreferrer"
             >
