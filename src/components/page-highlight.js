8585b59a00f9cfab138f8aea8e0a7faead5507cf
Emmanuel Luciano <emma.luciano11@gmail.com>
2020-01-03 07:03:13 -0400

    Fixes bug on external url in page highlight

--- src/components/page-highlight.js
+++ src/components/page-highlight.js
@@ -5,14 +5,14 @@ import { Link } from "gatsby";
 import styles from "./page-highlight.module.css";
 
 function PageHighlight({ header, subheader, url, urlTitle }) {
-  let isExternalUrl = url.indexOf("https") || url.indexOf("www");
+  let isExternalUrl = url.includes("https");
 
   return (
     <section className="py-7 px-2 bg-primary lg:py-10 lg:px-10">
       <div className="m-auto text-center w-11/12 max-w-6xl lg:w-10/12">
         <h3 className="text-h3 text-white font-extrabold mb-4">{header}</h3>
         <h4 className="text-h4 text-white mb-6">{subheader}</h4>
-        {isExternalUrl > 0 ? (
+        {isExternalUrl ? (
           <a
             href={url}
             className={`btn btn--inverse ${styles.cta}`}
