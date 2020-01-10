9c455c785a10803bc4d7f5209fc407c7bb9dbc6a
Ricardo A. Mercado <Ricardoamercado@gmail.com>
2020-01-10 06:52:58 -0400

    Minor styling to 404 page (#52)
    * Minor styling to 404 page
    
    * Updates  background and margin on 404 page
    
    Co-authored-by: Emmanuel Luciano <emma.luciano11@gmail.com>

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,27 @@
-import React from "react"
+import React from "react";
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import { GradientBackground, Layout, SEO } from "../components/index.js";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <GradientBackground className="flex flex-col items-center justify-center pt-13 lg:block">
+      <div className="flex flex-col text-center mt-10 lg:mt-20">
+        <h2 className="text-h2 font-bold text-white">NOT FOUND</h2>
+        <h4 className="text-h4 text-white mb-8">
+          You just hit a route that doesn&#39;t exist... the sadness.
+        </h4>
+        <div className="w-full m-auto mb-10 max-w-6xl lg:mb-20 lg:w-2/3">
+          <div className="flex flex-col items-center w-full mb-4">
+            <img
+              src="https://media.giphy.com/media/a9xhxAxaqOfQs/source.gif"
+              className="lg:w-1/2 md:w-full"
+            ></img>
+          </div>
+        </div>
+      </div>
+    </GradientBackground>
   </Layout>
-)
+);
 
-export default NotFoundPage
+export default NotFoundPage;
