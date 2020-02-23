80daaac9ceb5685aa3ee176d0a85d211792ba8b5
Raúl Negrón <rnegron@users.noreply.github.com>
2020-02-23 11:01:38 -0400

    Add Twitter link in COC page (#40)
    * Add twitter link to all ORGANIZERS constant
    
    * Extract Icon into its own component
    
    * Typo
    
    * Use new SocialAvatar component in COC
    
    * Update organizers constants to add social
    
    * add inline twitter SVG with viewBox
    
    * Revert SocialAvatar use
    
    * Update avatar component to use SVG
    
    * Revert extraction of Icon into component
    
    * Move margin to social mapping
    
    * Updates twitter size and adds links for founders
    
    Co-authored-by: Emmanuel Luciano <emma.luciano11@gmail.com>

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,12 +1,37 @@
 import React from "react";
 
+import Twitter from "../assets/twitter.inline.svg";
+
 import styles from "./avatar.module.css";
 
-export default function({ img, name }) {
+function getNetworkIcon(network) {
+  switch (network) {
+    case "twitter":
+      return <Twitter />;
+    default:
+      return;
+  }
+}
+export default function({ img, name, social = [] }) {
   return (
     <div className={styles.container}>
       <img className={`avatar m-auto ${styles.img}`} src={img} alt={name} />
       <p className="mt-3">{name}</p>
+      <div className="flex justify-center">
+        {social.map(s => {
+          return (
+            <a
+              key={s.link}
+              href={s.link}
+              className="w-6 mt-5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {getNetworkIcon(s.network)}
+            </a>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/constants.js
+++ src/constants.js
@@ -5,27 +5,47 @@ export const SLACK_LINK = "https://sopr-slack.herokuapp.com/";
 export const ORGANIZERS = [
   {
     name: "Emmanuel Luciano",
-    img: "/organizers/emmanuel-luciano.jpeg"
+    img: "/organizers/emmanuel-luciano.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/rucury" }]
   },
-  { name: "Raúl Negrón", img: "/organizers/raul-negron.jpeg" },
+  {
+    name: "Raúl Negrón",
+    img: "/organizers/raul-negron.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/rucury" }]
+  },
   {
     name: "Ricardo Mercado",
-    img: "/organizers/ricardo-mercado.jpeg"
+    img: "/organizers/ricardo-mercado.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/futoricky" }]
   },
-  { name: "Yamil Asusta", img: "/organizers/yamil-asusta.jpeg" }
+  {
+    name: "Yamil Asusta",
+    img: "/organizers/yamil-asusta.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/elbuo8" }]
+  }
 ];
 
 export const FOUNDERS = [
-  { name: "Axel Rivera", img: "/founders/axel-rivera.jpeg" },
+  {
+    name: "Axel Rivera",
+    img: "/founders/axel-rivera.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/arivera" }]
+  },
   {
     name: "Froilan Irizarry",
-    img: "/founders/froilan-irizarry.jpeg"
+    img: "/founders/froilan-irizarry.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/froidotdev" }]
   },
   {
     name: "Giovanni Collazo",
-    img: "/founders/giovanni-collazo.jpeg"
+    img: "/founders/giovanni-collazo.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/gcollazo" }]
   },
-  { name: "José Padilla", img: "/founders/jose-padilla.jpeg" }
+  {
+    name: "José Padilla",
+    img: "/founders/jose-padilla.jpeg",
+    social: [{ network: "twitter", link: "https://twitter.com/jpadilla_" }]
+  }
 ];
 
 const SECTIONS = [
