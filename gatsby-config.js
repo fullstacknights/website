3b8a4a511e22d014afa50cafad5cd992abd671ce
Raúl Negrón <raul.esteban.negron@gmail.com>
2023-03-05 13:36:33 -0400

    gatsby v5 upgrade

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,21 +36,6 @@ module.exports = {
           include: /\.inline\.svg$/
         }
       }
-    },
-    {
-      resolve: "gatsby-plugin-eslint",
-      options: {
-        test: /\.js$|\.jsx$/,
-        exclude: /(node_modules|.cache|public)/,
-        stages: ["develop", "build-javascript"],
-        options: {
-          emitWarning: true,
-          failOnError: false
-        }
-      }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ]
 };
