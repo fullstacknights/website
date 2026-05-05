872fc498b4bd38d786e2954bf16d3521fa635dc7
Raúl Negrón <raul.esteban.negron@gmail.com>
2026-05-05 15:34:44 -0400

    chore: bump date-fns to 4

--- src/components/upcoming-event.js
+++ src/components/upcoming-event.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 
 import { Link } from "gatsby";
-import format from "date-fns/format";
+import { format } from "date-fns";
 
 import Card from "./card";
 import ProfileCard from "./profile-card";
